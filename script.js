930353c56446e5ddcc9d112c72e13023fd113bc2
Jonathan <jonathankim1228@hotmail.com>
2015-11-04 16:09:58 -0800

    random colors

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 // Your JS goes here
-var body = document.getElementsByTagName('body')[0];
 
+var body = document.getElementsByTagName('body')[0];
+var color = 82;
 for(var row = 0; row < 9; row++){
 
 	for(var col = 0; col < 9; col++){
-	
-		var square = document.createElement('div');
-
+		color += 4;
 		if((col + row) % 2 === 0){
-			square.style.backgroundColor = "black";
+			var square = makeDiv(("'rgb(365, " + color + ", 100)'"));
 		}
 		else{
-			square.style.backgroundColor = "red";
+			var square = makeDiv("'rgb(365, " + color + ", 100)'");
 		}
-		square.style.width = "11.1%";
-		square.style.paddingBottom = "11.1%";
-		square.style.float = "left";
-
 		body.appendChild(square);			
 	}
 
 }
+function makeDiv(color) {
+
+	var square = document.createElement('div');
+	// modifies div
+	
+	square.style.backgroundColor = color;
+	square.style.width = "11.1%";
+	square.style.paddingBottom = "11.1%";
+	square.style.float = "left";
+	return square;
+				
+};
+function randomColor(){
+	var color = "#";
+	var colors = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+	for(var i = 0; i < 6; i++){
+		color += colors[Math.floor((Math.random() * 15) + 1)];
+	}
+	return color;
+
+
+}
+function colorizer_base(){
+	var colors = "'rgb(365, " + color + ", 100)'";
+	return colors;
+
+}
+​
+function colorizer_gradient(){
+	var colors = "'rgb(180, " + color + ", 19)'";
+	return colors;
+
+
+}
+​
